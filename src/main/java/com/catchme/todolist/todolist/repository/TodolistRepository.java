87b97678860f06b34d34ae5f5080827f42ba2ef0
eunju0127 <aoddmswn0127@gmail.com>
2025-05-25 00:53:57 +0900

    feat: [#3] todolist 수정, 삭제 api 수정
    - todolist 수정, 삭제 api 개발 수정

--- src/main/java/com/catchme/todolist/todolist/repository/TodolistRepository.java
+++ src/main/java/com/catchme/todolist/todolist/repository/TodolistRepository.java
@@ -2,10 +2,12 @@ package com.catchme.todolist.todolist.repository;
 
 import com.catchme.todolist.todolist.entity.TodolistEntity;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TodolistRepository extends JpaRepository<TodolistEntity, Long> {
     List<TodolistEntity> findByIsDeletedFalse();
+    Optional<TodolistEntity> findByIdAndIsDeletedFalse(Long id);
 }
